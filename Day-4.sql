3d6e10674e5c3de395cae33fc34c658586a71221
Eslam Mousa <EslamMousa123@gmail.com>
2024-08-18 20:04:41 +0300

    First commit

---
+++ Day-4.sql
@@ -0,0 +1,173 @@
+﻿select sum(salary) as Salary
+from Instructor
+
+
+select min(salary ) as MinSalary , max (salary) as MaxSalary
+from Instructor
+
+-----------------------------------------------------
+select count(*),count(st_id),count(st_lname),count(st_age)
+from Student
+----------------------------------------------------------------
+
+select avg(st_age)
+from Student
+
+select avg(isnull(st_age,0))
+from student
+
+select sum(st_age)/COUNT(*)
+from Student
+
+-------------------------------------------------------
+select sum(salary) , Dept_Id --  this  is code not run why ? becouse select show you one row ( sum salary ) and column of Dept_id
+from Instructor
+
+-- how to fixed this error ( one row of agg and column of Dept_id ) fixed this by grup by
+
+select sum(salary) , Dept_id
+from Instructor
+group by Dept_id
+
+select sum(salary), d.Dept_Id , dept_name
+from Instructor i inner join Department d
+on d.Dept_Id = i.Dept_Id
+group by d.Dept_Id , dept_name
+
+
+select avg(st_age) ,st_address ,dept_id
+from Student
+group by St_Address , Dept_Id
+
+
+select sum(salary), Dept_Id
+from Instructor
+group by Dept_Id
+
+
+select sum(salary), Dept_Id
+from Instructor
+where salary>1000
+group by Dept_Id
+having sum(salary) >100000
+
+
+select sum(salary), Dept_Id
+from Instructor
+where salary>1000
+group by Dept_Id
+having count(ins_id) < 5
+
+---------------------------------------
+-- Subqueries ( output of query as input in anthor query )
+
+select *
+from Student -- outer query            // inner query
+where St_Age<(select avg(st_age)from Student)
+
+
+select * , (select count(st_id) from Student )
+from student
+
+
+select dept_name 
+from Department
+where dept_id in (select distinct (dept_id)
+                 from Student
+				 where Dept_Id is not null )
+
+--------------------------------------------------
+
+-- subquery + DML
+delete from Stud_Course
+where st_id = 1
+
+
+delete from Stud_Course
+where st_id in (select st_id from Student where St_Address='cairo')
+
+--------------------------------------------------------------------------
+-- union family ( union all - union - intersect - except )
+-- number of columns select 1 = number of cloumns select 2
+-- union and intersct and except --> is distincit
+
+-- union --> summation rows of (st_fname + ins_name)
+select st_fname
+from Student
+union all 
+select ins_name
+from Instructor 
+
+
+select st_fname , st_id
+from Student
+union all
+select ins_name , ins_id
+from Instructor
+
+-- union same union all but union does distincit
+-- union --> distincit of rows ( acsinding name and remove Duplicated )
+select st_fname
+from Student 
+union 
+select ins_name
+from Instructor
+
+-- intersect --> names of student = names of instructor
+-- intersect is distincit (acsinding name and remove Duplicated)
+select st_fname 
+from Student
+intersect 
+select ins_name
+from Instructor
+
+-- except --> give me names in st_fname not equal ins_name
+-- names of Student not equal names of Instructor 
+-- expect --> is distincit
+select st_fname
+from Student
+except 
+select ins_name
+from Instructor
+
+-----------------------------------------------------------------------------------------
+select St_fname ,St_Age , dept_id
+from Student
+order by St_Address
+
+select st_fname , st_age , dept_id
+from Student
+order by 1 --  1 equal st_fname
+
+
+select St_fname ,St_Age , dept_id
+from Student
+order by dept_id , St_Age --Age بي order by بس لو لقي مجموعة طلبة في نفس القسم يبقا يعمل dept_Id هيرتب الاول ب  
+
+select St_fname ,St_Age , dept_id
+from Student
+order by dept_id asc , St_Age desc
+
+
+
+delete from Department Where dept_id =20 
+
+update Department set Dept_Id = 4000 where dept_id = 20
+
+--------------------------------------------------------------------------------------------
+
+-- builtin Functions
+-- Agg Functions - getdate() - isnulll - coalesce - Concat - Convert
+
+select year(getdate()) -- get the current year 
+
+select month(getdate()) -- get the current Month
+
+select substring (st_fname ,1,3)  -- substring of name of 3 chars
+from Student
+
+
+select db_name() -- how can i know my database
+
+
+select suser_name() -- how can i know user does login in database
---
+++ Day_3.sql
@@ -0,0 +1,179 @@
+﻿use ITI
+
+-- Cross join 
+select st_fname, dept_name
+from Student , Department
+
+select st_fname, dept_name
+from Student cross join Department
+
+----------------------------------------------------------------
+
+-- inner join
+
+select st_fname, dept_name
+from Student , Department  
+where Department.Dept_Id = Student.Dept_Id
+
+select st_fname, dept_name
+from Student inner join Department  
+on Department.Dept_Id = Student.Dept_Id
+
+select st_fname, dept_name
+from Student S , Department D  
+where S.Dept_Id = D.Dept_Id and St_Address = 'alex'
+order by Dept_Name Desc 
+
+--------------------------------------------------------------------
+
+-- Outer join (1- eft  2- Right 3- Full )
+
+-- left outer join 
+select st_fname, dept_name
+from Student S left outer join Department D  
+on S.Dept_Id = D.Dept_Id
+
+-- right outer join
+select st_fname, dept_name
+from Student S right outer join Department D  
+on S.Dept_Id = D.Dept_Id
+
+-- Full outer join
+select st_fname , dept_name
+from Student S full outer join Department D
+on S.Dept_Id = D.Dept_Id
+
+-------------------------------------------------------------------------------------
+
+-- self join
+select X.St_Fname as Sname ,Y.*
+from student X , Student Y -- Y --> perant ( have Primary Key ) - x --> Child ( have Forgin Key ) in Database
+where Y.St_Id= X.St_Id
+
+
+-----------------------------------------------------------------------------
+-- join Multi tables
+ select st_fname , Crs_Name ,grade
+ from Student S , Stud_Course SC , Course C
+ where S.St_Id = sc.St_Id and C.Crs_Id = SC.Crs_Id
+
+
+ select st_fname , Crs_Name ,grade ,dept_name
+ from Student S inner join Stud_Course SC 
+ on S.St_Id = sc.St_Id 
+ inner join  Course C
+ on C.Crs_Id = SC.Crs_Id
+ inner join Department D
+ on d.Dept_Id = s.St_Id
+
+ -----------------------------------------------------------------------------------------
+ -- join with DML
+ -- join update
+
+ -- update rows back from join
+ update Stud_Course
+       set Grade+=10
+ from student s , Stud_Course sc  
+ where s.St_Id = sc.St_Id and St_Address = 'Cairo'
+ -- from --> where --> update
+  update sc
+       set Grade+=10
+ from student s , Stud_Course sc
+ where s.St_Id = sc.St_Id and St_Address = 'Cairo'
+
+ ----------------------------------------------------
+  select st_fname
+ from Student 
+ where St_Fname is not null
+
+-- how can i replace null for value
+
+select isnull (st_fname , '')  -- replace null to space
+from Student
+
+select isnull(st_fname , 'has no name ') -- replace null to has no name
+from Student
+
+
+select isnull(st_fname , st_lname ) as new_name -- replace null to anthor Column
+from Student
+
+
+select coalesce(st_fname , st_lname,st_address ,'No Data') -- Multireplace 
+ 
+ ----------------------------------------------------------------------------------------
+
+ -- how can i concat two column in one column
+ select st_fname +' '+ st_lname as full_name
+ from Student
+
+ -- Concat two column but two column not the same type
+ select st_fname + ' ' + convert(varchar(2) , st_age)
+ from Student
+
+ select 'studname = '+ st_fname +'   &age = '+convert(varchar(2) , st_age)
+ from Student
+
+ -- لو عملت كونكات ل عمودين وفيه عمود فيه قيمة فاضية قيمة الصف كلها هتبقا فاضية
+
+select isnull('studname = '+ st_fname ,'' )+'   &age = '+convert(varchar(2) , isnull(st_age , 0))
+from Student
+
+-- use to function Concat 
+-- what will do function concat ( changing every column to string if (date - int - float) and replace null value to Empty string or age
+select concat(st_fname ,' ', st_age) 
+from student
+
+----------------------------------------------------------------------------------------
+select * 
+from Student 
+where st_fname = 'ahmed'
+
+select * 
+from Student 
+where st_fname like 'ahmed'
+
+select *
+from Student
+where St_Fname like 'a%' -- select every name the st_fname start 'a'
+
+
+select *
+from Student
+where St_Fname like '%a' -- selevt every name the st_fname end 'a'
+
+select * 
+from Student 
+where St_Fname like '%a%' -- select every name containe 'a'
+
+
+select * 
+from Student 
+where st_fname like '_a%' -- s    elect every name the second char has 'a'
+
+-- 'a%h' string first char has 'a' and last char 'h'
+-- '%a_' string last second has 'a'
+-- 'ahm%' string start ahm
+-- '[ahm]%' string start 'a' or 'h' or 'm' 
+-- '[^ahm]%' string not start 'a' or 'h' or 'm'
+-- '[a-h]%'  string  start of range of 'a' to 'h'
+-- '[^a-h]%' string not start of range of 'a' to 'h'
+-- '%[%]'  string end '%'
+-- '%[_]%' string the middle has '_'  ahmed-ail  - eman_omar
+-- '[_]%[_]' string start '_' and end '-'  -eslam-
+
+
+select st_fname ,dept_id , st_age
+from Student
+order by St_Address -- Column order by should'nt be in select
+
+select st_fname , dept_id , st_age
+from Student
+order by 1    -- order by using the first column in select ( st_fname)
+
+
+select st_fname , dept_id , st_age 
+from Student
+order by dept_id , st_age 
+-- هيرتب ب القسم ولو كان القسم فيه قيم متكررة هيرتب ب العمر 
+
